65d0653e26573de4623f1f448ad75b1456509012
Nebojsa Mrkic <nmrkic@surveymonkey.com>
2021-12-28 08:14:39 +0100

    -- adding settings for mac

---
+++ .config/fish/conf.d/virtualfish-loader.fish
@@ -0,0 +1,8 @@
+set -g VIRTUALFISH_VERSION 2.5.4
+set -g VIRTUALFISH_PYTHON_EXEC /Library/Frameworks/Python.framework/Versions/3.10/bin/python3.10
+source /Library/Frameworks/Python.framework/Versions/3.10/lib/python3.10/site-packages/virtualfish/virtual.fish
+source /Library/Frameworks/Python.framework/Versions/3.10/lib/python3.10/site-packages/virtualfish/auto_activation.fish
+source /Library/Frameworks/Python.framework/Versions/3.10/lib/python3.10/site-packages/virtualfish/compat_aliases.fish
+source /Library/Frameworks/Python.framework/Versions/3.10/lib/python3.10/site-packages/virtualfish/projects.fish
+source /Library/Frameworks/Python.framework/Versions/3.10/lib/python3.10/site-packages/virtualfish/environment.fish
+emit virtualfish_did_setup_plugins
