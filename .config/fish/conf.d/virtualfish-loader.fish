f4111ca94a1aa49c1e98eca083eda8a32f20a643
Nebojsa Mrkic <mrkic.nebojsa@gmail.com>
2024-02-13 09:50:45 +0100

    -- new linux

---
+++ .config/fish/conf.d/virtualfish-loader.fish
@@ -0,0 +1,8 @@
+set -g VIRTUALFISH_VERSION 2.5.5
+set -g VIRTUALFISH_PYTHON_EXEC /usr/bin/python3
+source /home/nebojsa/.local/lib/python3.10/site-packages/virtualfish/virtual.fish
+source /home/nebojsa/.local/lib/python3.10/site-packages/virtualfish/compat_aliases.fish
+source /home/nebojsa/.local/lib/python3.10/site-packages/virtualfish/environment.fish
+source /home/nebojsa/.local/lib/python3.10/site-packages/virtualfish/auto_activation.fish
+source /home/nebojsa/.local/lib/python3.10/site-packages/virtualfish/projects.fish
+emit virtualfish_did_setup_plugins
